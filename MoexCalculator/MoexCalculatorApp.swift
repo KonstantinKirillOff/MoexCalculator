358b7f848b8ecb628a8cc5a7fc98e00a8bff1f5e
Konstantin Kirillov <kirilloffkostya@gmail.com>
2024-02-23 12:15:01 +0300

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by Konstantin Kirillov on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
