1ee1954f5abc65698babe284b4d3a58cc5726a7d
Konstantin Kirillov <kirilloffkostya@gmail.com>
2024-02-24 14:26:55 +0300

    add view

--- MoexCalculator/CalculatorScreen/CalculatorView.swift
+++ MoexCalculator/CalculatorScreen/CalculatorView.swift
@@ -6,3 +6,65 @@
 //
 
 import SwiftUI
+
+struct CalculatorView: View {
+    @ObservedObject var viewModel: CalculatorViewModel
+    
+    var numberFormatter: NumberFormatter = {
+       let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.usesGroupingSeparator = false
+        nf.maximumFractionDigits = 2
+        return nf
+    }()
+    
+    var body: some View {
+        
+        List {
+            HStack {
+                VStack {
+                    Text(viewModel.topCurrency.flag)
+                        .font(.system(size: 200))
+                        .minimumScaleFactor(0.01)
+                        .aspectRatio(1, contentMode: .fit)
+                    Text(viewModel.topCurrency.rawValue)
+                        .font(.title2)
+                }
+                .frame(height: 100)
+                
+                TextField("", value: $viewModel.topAmount, formatter: numberFormatter)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.trailing)
+                    .minimumScaleFactor(0.5)
+                    .keyboardType(.numberPad)
+            }
+            
+            HStack {
+                VStack {
+                    Text(viewModel.bottomCurrency.flag)
+                        .font(.system(size: 200))
+                        .minimumScaleFactor(0.01)
+                        .aspectRatio(1, contentMode: .fit)
+                    Text(viewModel.bottomCurrency.rawValue)
+                        .font(.title2)
+                }
+                .frame(height: 100)
+                
+                TextField("", value: $viewModel.bottomAmount, formatter: numberFormatter)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.trailing)
+                    .minimumScaleFactor(0.5)
+                    .keyboardType(.numberPad)
+            }
+        }
+        
+        
+        
+    }
+}
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView(viewModel: CalculatorViewModel())
+    }
+}
